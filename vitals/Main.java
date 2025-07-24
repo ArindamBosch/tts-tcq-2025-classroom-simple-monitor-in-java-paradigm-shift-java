3b335c85fb75dff58e654515c8810285af7421d6
Dutta Arindam (MS/EMT3-XC) <Arindam.Dutta@in.bosch.com>
2025-07-24 09:26:05 +0530

    removed if and reduced CCN to 1

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,39 +2,33 @@ package vitals;
 
 public class Main {
 
-    static boolean isTemperatureOk(float temperature) {
-        if (temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        }
-        return true;
+     public static boolean isTemperatureOk(float temperature) {
+        return temperature >= 0 && temperature <= 45;
     }
 
-    static boolean isSocOk(float soc) {
-        if (soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        }
-        return true;
+    public static boolean isSocOk(float soc) {
+        return soc >= 20 && soc <= 80;
     }
 
-    static boolean isChargeRateOk(float chargeRate) {
-        if (chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+    public static boolean isChargeRateOk(float chargeRate) {
+        return chargeRate <= 0.8;
     }
 
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        return isTemperatureOk(temperature) &&
-               isSocOk(soc) &&
-               isChargeRateOk(chargeRate);
+    public static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+        boolean tempOk = isTemperatureOk(temperature);
+        boolean socOk = isSocOk(soc);
+        boolean chargeRateOk = isChargeRateOk(chargeRate);
+
+        if (!tempOk) System.out.println("Temperature is out of range!");
+        if (!socOk) System.out.println("State of Charge is out of range!");
+        if (!chargeRateOk) System.out.println("Charge Rate is out of range!");
+
+        return tempOk && socOk && chargeRateOk;
     }
 
     public static void main(String[] args) {
         assert(batteryIsOk(25, 70, 0.7f));
         assert(!batteryIsOk(50, 85, 0.0f));
-        System.out.println("Some more tests needed");
+        System.out.println("All tests passed");
     }
 }
